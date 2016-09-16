450ee3d2b251b7026a50bee04e4155da553ca72f
sareduhh <sarahe.kaminski@gmail.com>
2016-09-16 14:55:18 -0400

    First Commit

---
+++ VirtualPet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        //Fields
+
+
+        //Properties (at least three)
+
+        //Constructors
+
+        //Methods (at least three)
+        //have a Tick Method (see Birthday example)
+        public void Tick()
+        {
+
+        }
+        public void Hunger()
+        {
+
+        }
+        public void Thirst()
+        {
+
+        }
+
+    }
+}
